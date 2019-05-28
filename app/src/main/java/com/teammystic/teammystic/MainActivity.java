a161da55262a55a7cb3d0f28f6f9fba33f07f900
Kershawn Franklyn <kershawn.franklyn@my.uwi.edu>
2019-05-27 21:26:58 -0400

    First commit for Team Mystic

---
+++ app/src/main/java/com/teammystic/teammystic/MainActivity.java
@@ -0,0 +1,29 @@
+package com.teammystic.teammystic;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button mTravelling;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTravelling = (Button) findViewById(R.id.travelling);
+
+        mTravelling.setOnClickListener(new View.OnClickListener(){
+            @Override
+                    public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, LoginActivty.class);
+                startActivity(intent);
+                finish();
+
+
+            }
+        });
+    }
+}
